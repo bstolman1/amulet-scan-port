18962a9f7e4fdeeac0205dba23eeb21f5c50fe56
bstolman1 <82995922+bstolman1@users.noreply.github.com>
2025-12-30 14:32:17 -0500

    Change initial migration ID from 0 to 1

--- scripts/ingest/fetch-backfill-parquet.js
+++ scripts/ingest/fetch-backfill-parquet.js
@@ -434,7 +434,7 @@ async function detectMigrations() {
   console.log("🔎 Detecting available migrations via /v0/backfilling/migration-info");
 
   const migrations = [];
-  let id = 0;
+  let id = 1;
 
   while (true) {
     try {
